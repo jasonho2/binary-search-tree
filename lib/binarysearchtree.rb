7b99849d8e84680108964026334e0a6e8f8229cf
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-02-01 16:06:23 -0800

    Finish build_tree method with pretty_print

--- lib/binarysearchtree.rb
+++ lib/binarysearchtree.rb
@@ -1,10 +1,31 @@
 require_relative 'node.rb'
+require_relative 'merge_sort.rb'
 
 class Tree
-  attr_accessor :root, :left, :right
+  attr_accessor :root#, :left, :right
 
   def initialize(array)
     @root = build_tree(array)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+    array = merge_sort(array).uniq
+    
+    mid = array.size / 2
+    
+    root = Node.new(array[mid])
+    root.left = build_tree(array[0...mid])
+    root.right = build_tree(array[mid+1...])
+
+    root
+  end
+
+  # print method provided by student via Discord
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
 end
